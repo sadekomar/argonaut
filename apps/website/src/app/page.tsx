e018cb44de15b737afdb9ddf1c477778a982c96b
Omar Sadek <sadekm.omar@gmail.com>
2025-09-02 23:17:49 +0300

    Update button text from GitHub icon to Hello

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <div>
         <button>
-          <Image src={'/github.svg'} width={24} height={24} alt="GitHub" />
+          Hello
         </button>
       </div>
       <Image src={'/argonaut-gradient.png'} width={2160} height={1080} alt="Argonaut" />
