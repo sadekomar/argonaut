36a097d0b25902569c23154d2e5161f891646425
AdelMohamedS <sadekm.adel@gmail.com>
2025-09-11 00:09:09 +0300

    added stats section

--- apps/website/src/app/(home)/(components)/stats.tsx
+++ apps/website/src/app/(home)/(components)/stats.tsx
@@ -1,17 +1,51 @@
 import React from "react";
+import Image from "next/image";
+import StatsGrid from "../../../components/stats-fade";
+import CountUp from "@/components/count-up";
 
 export function Stats() {
   return (
     <div className="backgroundGrayGradient">
-      <div className="flex w-full max-w-300 flex-row justify-between">
-        <h1 className="heading1 !max-w-100">
-          Proven Track Record of Excellence
-        </h1>
-        <div className="flex flex-col">
-          <div className="heading1 !text-[160px]">100%</div>
-          <div className="description1">Client Satisfaction</div>
-        </div>
+      <div className="base:grid-cols-2 base:justify-between base:[&>*:nth-child(even)]:justify-self-end max-base:[&>*:not(:first-child)]:justify-self-center grid w-full max-w-300 grid-cols-1 gap-11 sm:gap-16 [&>*:nth-child(n+3)]:max-h-57">
+        <SectionHeading />
+        <BigStat />
+        <Graph />
+        <StatsGrid />
       </div>
     </div>
   );
+
+  function SectionHeading() {
+    return (
+      <h1 className="heading1 !max-w-150">Proven Track Record of Excellence</h1>
+    );
+  }
+
+  function BigStat() {
+    return (
+      <div className="base:min-w-103 flex flex-col">
+        {/* <div className="heading1 !text-9xl xl:!text-[160px]">100%</div> */}
+        <CountUp
+          to={100}
+          duration={0.5}
+          suffix="%"
+          className="heading1 !text-9xl xl:!text-[160px]"
+        />
+        <div className="description1">Client Satisfaction</div>
+      </div>
+    );
+  }
+
+  function Graph() {
+    return (
+      <Image
+        width={0}
+        height={0}
+        src="/chart.png"
+        alt="asd"
+        className="h-auto w-full object-contain"
+        sizes="100vw"
+      />
+    );
+  }
 }
