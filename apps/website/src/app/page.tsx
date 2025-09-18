7c2281cccc3ffd39c5a57c7cc028d9a1b3a4218d
Omar Sadek <sadekm.omar@gmail.com>
2025-09-18 16:11:59 +0300

    removed seminar countdown

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
     <>
       <Hero />
       <TwoBusinessArms />
-      <SeminarCountdown />
       <StrategicPartnerships />
       <Services />
       {/* <Stats /> */}
