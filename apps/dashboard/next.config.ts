6c3b5ee62cb66278126b5431b56c8eb25659ff29
Omar Sadek <sadekm.omar@gmail.com>
2025-10-25 21:52:12 +0300

    Revert "vercel"
    This reverts commit 687b3d6143a24234bb4070e5fec8be35f663dcb9.

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/client"],
+  /* config options here */
 };
 
 export default nextConfig;
